d8fd9ac27af821c9c112148f1983564a63f10c13
Charlie Broom <broomc425@gmail.com>
2019-11-16 21:13:00 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ def print_first_directors_movie_titles
   
   while index < ss_movies.length do
     titles = ss_movies[index][:title]
-    puts titles
+    puts titles 
     index += 1
   end
 end
